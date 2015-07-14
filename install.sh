8a069f2afb9d920abaa0416eb383efee2805884d
nicki6 <nicolai.tesela@gmail.com>
2015-07-14 14:12:21 -0500

    install2

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cd $FF_DIR
 curl -O "https://raw.githubusercontent.com/nikisix/ff/master/ff.sh"
 curl -O "https://raw.githubusercontent.com/nikisix/ff/master/ff.py"
 curl -O "https://raw.githubusercontent.com/nikisix/ff/master/README"
-cat "alias ff='. $FF_DIR/ff.sh'" >> ~/.bashrc
+cat 'alias ff=". /opt/ff/ff.sh"' >> ~/.bashrc
