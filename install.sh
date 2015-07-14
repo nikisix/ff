224e75642f0ebf2c0e2dc6688c1b43cf1c48fc2d
nicki6 <nicolai.tesela@gmail.com>
2015-07-14 16:40:27 -0500

    install7

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ curl -O "https://raw.githubusercontent.com/nikisix/ff/master/ff.py"
 curl -O "https://raw.githubusercontent.com/nikisix/ff/master/README"
 echo "alias ff='. $FF_DIR/ff.sh'" >> ~/.bashrc
 chmod 775 $FF_DIR/ff.py $FF_DIR/ff.sh
-source ~/.bashrc
+#http://stackoverflow.com/questions/670191/getting-a-source-not-found-error-when-using-source-in-a-bash-script
+. ~/.bashrc #must be '.' not source. '.' is for bash, source is for csh
