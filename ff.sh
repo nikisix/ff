4f524e3160ff28aa204ab6168e91732c2a7261fb
nicki6 <nicolai.tesela@gmail.com>
2015-07-14 15:39:01 -0500

    install4

--- ff.sh
+++ ff.sh
@@ -1,11 +1,13 @@
 #FF_DIR=/opt/ff
 # FF_USER=`id | sed 's/[^(]*(//;s/).*//'`
+# echo $FF_USER
 # FF_PWD_DIR="${TMPDIR-/tmp}ff-$FF_USER"
 # mkdir -p $FF_PWD_DIR
 # FF_TEMP_FILE="$FF_PWD_DIR/ff.pwd.$$"
-FF_DIR="~/.ff"
+FF_DIR=~/.ff
 FF_TEMP_FILE="$FF_DIR/ff_temp.txt"
 touch $FF_TEMP_FILE
+#touch ~/.ff/ff_temp.txt
 python $FF_DIR/ff.py "$FF_TEMP_FILE" "$@"
 
 if test -r "$FF_TEMP_FILE"; then
@@ -18,3 +20,4 @@ fi
 
 rm -f "$FF_TEMP_FILE"
 unset FF_TEMP_FILE
+# unset FF_USER
