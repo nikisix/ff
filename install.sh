45148a5a49e41c29a99c15a4926a01e4db112445
nicki6 <nicolai.tesela@gmail.com>
2015-07-14 14:08:30 -0500

    a

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir -p /opt/ff
 set FF_DIR=/opt/ff
 cd $FF_DIR
-curl -o "https://raw.githubusercontent.com/nikisix/ff/master/ff.sh"
-curl -o "https://raw.githubusercontent.com/nikisix/ff/master/ff.py"
-curl -o "https://raw.githubusercontent.com/nikisix/ff/master/README"
+curl -O "https://raw.githubusercontent.com/nikisix/ff/master/ff.sh"
+curl -O "https://raw.githubusercontent.com/nikisix/ff/master/ff.py"
+curl -O "https://raw.githubusercontent.com/nikisix/ff/master/README"
 cat "alias ff='. $FF_DIR/ff.sh'" >> ~/.bashrc
