0f4307dc8056bd958cc63414e0acc9d9356306ce
nicki6 <nicolai.tesela@gmail.com>
2015-07-15 10:42:33 -0500

    added colors and child-parent location memory

--- ff.py
+++ ff.py
@@ -7,6 +7,23 @@ import termios
 import tty
 import subprocess
 from subprocess import Popen
+
+HELP_MSG="""
+ _____         _     _____ _ _           
+|  ___|_ _ ___| |_  |  ___(_) | ___  ___ 
+| |_ / _` / __| __| | |_  | | |/ _ \/ __|
+|  _| (_| \__ \ |_  |  _| | | |  __/\__ \\
+|_|  \__,_|___/\__| |_|   |_|_|\___||___/
+author: nickiVI 
+commands:
+h - file left
+j - (down) enter dir
+k - up dir
+l - file right
+q - exit without changing dirs
+enter - change dir to currently selected and exit
+e - preview the file (with less)
+"""
 # A basic clear screen and cursor removal for program start...
 os.system("clear")
 
@@ -26,22 +43,6 @@ line=1
 position=0
 inkey_buffer=1
 score=0
-HELP_MSG="""
- _____         _     _____ _ _           
-|  ___|_ _ ___| |_  |  ___(_) | ___  ___ 
-| |_ / _` / __| __| | |_  | | |/ _ \/ __|
-|  _| (_| \__ \ |_  |  _| | | |  __/\__ \\
-|_|  \__,_|___/\__| |_|   |_|_|\___||___/
-author: nickiVI 
-commands:
-h - file left
-j - (down) enter dir
-k - up dir
-l - file right
-q - exit without changing dirs
-enter - change dir to currently selected and exit
-e - preview the file (with less)
-"""
 def inkey():
     fd=sys.stdin.fileno()
     remember_attributes=termios.tcgetattr(fd)
@@ -50,29 +51,62 @@ def inkey():
     termios.tcsetattr(fd, termios.TCSADRAIN, remember_attributes)
     return character
 
-def formatdir(dirlist, position):
-    i = 0; ret = []
+
+
+# Color table (wikipedia/ANSI_escape_code). 
+# foreground = 30+x  background = 40+x, where x is..
+# Intensity	0	1	2	3	4	5	6	7
+# Normal	Black	Red	Green	Yellow	Blue	Magenta	Cyan	White
+# Bright	Black	Red	Green	Yellow	Blue	Magenta	Cyan	White
+# i.e. print CSI+"31;40m" + "Colored Text" + reset + "Non-Colored Text"
+colormap = {'black':0,'red':1,'green':2,'yellow':3,'blue':4,'magenta':5,'cyan':6,'white':7}
+fg_colormap = {'black':30,'red':31,'green':32,'yellow':33,'blue':34,'magenta':35,'cyan':36,'white':37}
+def colorize(text, foreground_color):
+    CSI="\x1B["
+    reset=CSI+"m"
+    return CSI + str(fg_colormap[foreground_color])+'m'+text+reset
+
+def format_dir(dirlist):
     for l in dirlist:
-        if i == position: ret.append('*[['+l+']]*')
-        else: ret.append(l)
+        if os.path.isdir('../'+l): print colorize(l,'blue'),
+        else: print l,
+
+def format_cur_dir(dirlist, position):
+    i = 0;
+    for l in dirlist:
+        if i == position: print colorize('*[['+l+']]*', 'magenta'),
+        else: 
+            if os.path.isdir('./'+l): print(colorize(l,'blue')),
+            else: print(l),
         i+=1
-    return ret 
+
+def move_dir(direction, child, current, parent):
+    if direction == 'up':
+        child = current
+        current = parent
+        parent = 0
+    elif direction == 'down':
+        parent = current
+        current = child
+        child = 0
+    else: print "Error bad direction"
+    return (child,current,parent)
 
 if __name__ == '__main__':
-    position = 0
+    child=0; position = 0; parent=0; 
     while 1: 
         os.system("clear")
         print HELP_MSG
         curdir = os.listdir('.')
         print "PARENT DIR"
         print "||||||||||||||||||||||||||||||||||||||||||||||||||"
-        print os.listdir('..')
-        print "||||||||||||||||||||||||||||||||||||||||||||||||||\n"
+        format_dir(os.listdir('..'))
+        print "\n||||||||||||||||||||||||||||||||||||||||||||||||||\n"
         print "CURRENT DIR"
         print os.getcwd()
         print "||||||||||||||||||||||||||||||||||||||||||||||||||"
-        print formatdir(curdir, position)
-        print "||||||||||||||||||||||||||||||||||||||||||||||||||"
+        format_cur_dir(curdir, position)
+        print "\n||||||||||||||||||||||||||||||||||||||||||||||||||"
         k=inkey()
         if   k == 'q': break
         elif k == 'l': 
@@ -80,13 +114,14 @@ if __name__ == '__main__':
         elif k == 'h': 
             if position > 0: position -= 1
         elif k == 'k':
+            #TODO add an index check here to make sure that a new parent is at least as long as the previous parent
             os.chdir('..')
-            position = 0
+            (child, position, parent) = move_dir('up',child, position, parent)
         elif k == 'j':
             if len(curdir) > 0: #we have some files/directories in this location
                 if os.path.isdir('./'+curdir[position]):
                     os.chdir('./'+curdir[position])
-                    position = 0
+                    (child, position, parent) = move_dir('down',child, position, parent)
         elif k == 'e':
             os.system("less "+'./'+curdir[position])
         elif k == '\r':
