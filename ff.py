defe3b36f509332b39bc5a85eaf7a5f52b94b544
nicki6 <nicolai.tesela@gmail.com>
2015-07-23 21:36:57 -0500

    operation 2d success

--- ff.py
+++ ff.py
@@ -9,22 +9,17 @@ import tty
 import subprocess
 from subprocess import Popen
 
-HELP_MSG="""
- _____         _     _____ _ _           
-|  ___|_ _ ___| |_  |  ___(_) | ___  ___ 
-| |_ / _` / __| __| | |_  | | |/ _ \/ __|
-|  _| (_| \__ \ |_  |  _| | | |  __/\__ \\
+HELP_MSG="""\
+ _____         _     _____ _ _              h - file left 
+|  ___|_ _ ___| |_  |  ___(_) | ___  ___    j - file down
+| |_ / _` / __| __| | |_  | | |/ _ \/ __|   k - file up
+|  _| (_| \__ \ |_  |  _| | | |  __/\__ \\  l - file right
 |_|  \__,_|___/\__| |_|   |_|_|\___||___/
 author: nickiVI 
 commands:
-h - file left
-j - (down) enter dir
-k - up dir
-l - file right
 q - exit without changing dirs
 enter - change dir to currently selected and exit
-e - preview the file (with less)
-"""
+e - preview the file (with less)"""
 # A basic clear screen and cursor removal for program start...
 os.system("clear")
 
@@ -72,76 +67,70 @@ CSI_Reset=CSI+"m"
 def colorize(text, foreground_color):
     return CSI + str(fg_colormap[foreground_color])+'m'+text+CSI_Reset
 
-# def format_dir(dirlist):
-#     for l in dirlist:
-#         if os.path.isdir('../'+l): print colorize(l,'blue'),
-#         else: print l,
-def print2d(dirlist, num_cols, files_per_col):
-    
-def format_dir(dirlist):
-#     h, w  = terminal_size()
-#     max_file_len = max([len(l) for l in dirlist])
-#     num_columns = w/max_file_len
-#     files_per_column = math.ceil(len(dirlist)/num_columns)
-#     print2d(dirlist, num_columns, files_per_column)
-    for l in dirlist:
-        if os.path.isdir('../'+l): print colorize(l,'blue'),
-        else: print l,
-
-def format_cur_dir(dirlist, position):
-    i = 0;
-    for l in dirlist:
-        if i == position: print colorize('*[['+l+']]*', 'magenta'),
-        else: 
-            if os.path.isdir('./'+l): print(colorize(l,'blue')),
-            else: print(l),
+def color_and_pad_filenames(dirlist, position, rel_level):
+    max_file_len = max([len(l) for l in dirlist]) + 2 if len(dirlist) > 0 else 1
+    ret=[]; i = 0
+    for f in dirlist:
+        num_spaces = max_file_len - len(str(f))
+        if position==i:    f = colorize(f,'magenta')
+        else:    f = colorize(f,'blue') if os.path.isdir('../'*rel_level+f) else f
+        ret.append(f+'.'*num_spaces)
         i+=1
+    return ret
 
-def move_dir(direction, child, current, parent):
-    if direction == 'up':
-        child = current
-        current = parent
-        parent = 0
-    elif direction == 'down':
-        parent = current
-        current = child
-        child = 0
-    else: print "Error bad direction"
-    return (child,current,parent)
+#rel_level - relative folder level to where you are. 0 is current, 1 is parent
+def print2d(dirlist, num_cols, files_per_col,  position=-1, rel_level=0):
+    fray = [] #"filearray" 2d array of files in the current directory
+    dirlist = color_and_pad_filenames(dirlist, position, rel_level) 
+    for i in range(num_cols):  
+        start = i*files_per_col
+        end   = i*files_per_col+files_per_col if len(dirlist) >= i*files_per_col+files_per_col else len(dirlist)
+        if i == num_cols:   end = len(dirlist)
+        fray.append(dirlist[start:end])
+    max_rows = max([len(a) for a in fray])
+    for r in range(max_rows):
+        print ''.join([a[r] for a in fray if r<len(a)])
 
-def print_display():
+def print_display(num_cols, files_per_col):
     os.system("clear")
 #     print HELP_MSG
-    curdir = os.listdir('.')
-    print "PARENT DIR"
-    print "||||||||||||||||||||||||||||||||||||||||||||||||||"
-    format_dir(os.listdir('..'))
-    print "\n||||||||||||||||||||||||||||||||||||||||||||||||||\n"
-    print "CURRENT DIR"
+    print "|||PARENT DIR|||||||||||||||||||||||||||||||||||||"
+    print2d(os.listdir('..'), num_cols, files_per_col, position=-1, rel_level=1)
+    print "\n||||||||||||||||||||||||||||||||||||||||||||||||||"
     print os.getcwd()
-    print "||||||||||||||||||||||||||||||||||||||||||||||||||"
-    format_cur_dir(curdir, position)
+    print "||||CURRENT DIR|||||||||||||||||||||||||||||||||||"
+    print2d(os.listdir('.'), num_cols, files_per_col, position=position, rel_level=0)
     print "\n||||||||||||||||||||||||||||||||||||||||||||||||||"
 
 if __name__ == '__main__':
     child=0; position = 0; parent=0; 
+    h, w  = terminal_size()
     while 1: 
-        print_display()
+        curdir = os.listdir('.')
+        #TODO can only see one out of every two directories
+        max_file_len = max([len(l) for l in curdir]) if len(curdir) > 0 else 1 #don't divide by 0
+        num_cols = int(w/max_file_len)
+        files_per_col = int(math.ceil(float(len(curdir))/num_cols))
+        print_display(num_cols, files_per_col)
         k=inkey()
         if   k == 'q': break
         elif k == 'l': 
-            if position < len(curdir)-1: position += 1
+            if position+files_per_col < len(curdir):  position = position+files_per_col
         elif k == 'h': 
-            if position > 0: position -= 1
-        elif k == 'k':
-            #TODO add an index check here to make sure that a new parent is at least as long as the previous parent
+            if position-files_per_col >= 0: position = position-files_per_col
+        elif k == 'j': 
+            if position+1 < len(curdir):  position = position+1
+        elif k == 'k': 
+            if position-1 >= 0: position = position-1
+        elif k == 'f':
+            #TODO add an index check here to make sure that a new parent is at least as long as theprevious parent
             os.chdir('..')
-            (child, position, parent) = move_dir('up',child, position, parent)
-        elif k == 'j':
+            position = len(os.listdir('.'))/2
+        elif k == 'd':
             if len(curdir) > 0: #we have some files/directories in this location
                 if os.path.isdir('./'+curdir[position]):
                     os.chdir('./'+curdir[position])
-                    (child, position, parent) = move_dir('down',child, position, parent)
+                    position = len(os.listdir('.'))/2
         elif k == 'e':
             os.system("less "+'./'+curdir[position])
         elif k == '\r':
@@ -149,6 +138,4 @@ if __name__ == '__main__':
             print str(os.getcwd())
             with open(sys.argv[1],'w+') as tempfile:
                 tempfile.write(os.getcwd())
-            #subprocess.Popen(["open", str(os.getcwd)], shell=True)
-            #os.system(str("cd "+os.getcwd()))
             break
